539edd9a1f7a9dc6ba3550110b1dd8d3224f8f09
ValentinaAkhmolina <vahmolina@delight2000.com>
2016-02-18 18:48:04 +0300

    Initial

---
+++ FaceDetection/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace FaceDetection
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MediaCapture mediaCapture;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        async private void InitCamera_Click(object sender, RoutedEventArgs e)
+        {
+            ImageEncodingProperties imgFormat = ImageEncodingProperties.CreateJpeg();
+
+            // create storage file in local app storage
+            StorageFile file = await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                "TestPhoto.jpg",
+                CreationCollisionOption.GenerateUniqueName);
+
+            // take photo
+            await mediaCapture.CapturePhotoToStorageFileAsync(imgFormat, file);
+
+            // Get photo as a BitmapImage
+            BitmapImage bmpImage = new BitmapImage(new Uri(file.Path));
+
+            if (bmpImage!=null)
+            {
+                //переходим к другой странице
+            }
+
+            // imagePreivew is a <Image> object defined in XAML
+            //imagePreivew.Source = bmpImage;
+        }
+
+        async private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            mediaCapture = new MediaCapture();
+            await mediaCapture.InitializeAsync();
+        }
+    
+    }
+}
